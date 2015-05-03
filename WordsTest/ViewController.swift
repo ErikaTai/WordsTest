5e000ecc0d655440616f44786d7ca8f2db2b3446
KumiHamazaki <kumi.hamazaki.1217@gmail.com>
2015-05-04 08:29:56 +0900

    [add]答えを確認する昨日の追加
    日本語が表示されている状態で、「回答する」だけでなく、「答えを見る」という選択肢を増やした
    答えが表示されているうちは、「回答する」ボタンは使用不可とした

--- WordsTest/ViewController.swift
+++ WordsTest/ViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var lblQuestion: UILabel!
     @IBOutlet weak var lblJudge: UILabel!
+    @IBOutlet weak var lblAnswer: UILabel! //追加　答え表示用ラベル
     @IBOutlet weak var txtAnswer: UITextField!
     @IBOutlet weak var btnAnswer: UIButton!
     @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var btnShowAns: UIButton! //追加　「答えを見る」ボタン
     
     var words: Words = Words()
     var rand: Int = 0
@@ -30,10 +32,22 @@ class ViewController: UIViewController {
             self.lblJudge.text = "正解!"
             self.btnAnswer.hidden = true
             self.btnNext.hidden = false
+            self.btnShowAns.hidden = true //追加
         } else {
             self.lblJudge.text = "不正解"
         }
     }
+    
+    @IBAction func showAnswer(sender: AnyObject){ //追加　「答えを見る」ボタンの処理
+        self.lblJudge.hidden = true
+        self.btnAnswer.hidden = true
+        self.lblAnswer.hidden = false
+        self.lblAnswer.text = words.getEnglish(rand)
+        self.btnNext.hidden = false
+        self.btnShowAns.hidden = true
+        
+    }
+
     
     @IBAction func nextQuestion(sender: AnyObject) {
         newQuestion()
@@ -41,10 +55,11 @@ class ViewController: UIViewController {
     
     func newQuestion() {
         rand = Int(arc4random() % words.getCount() + 0)
-        
+        self.lblAnswer.hidden = true //追加
         self.lblJudge.hidden = true
         self.btnAnswer.hidden = false
         self.btnNext.hidden = true
+        self.btnShowAns.hidden = false //追加
         self.txtAnswer.text = ""
         
         self.lblQuestion.text = words.getJapanese(rand)
